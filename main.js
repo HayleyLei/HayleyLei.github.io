927e3bdda494f3385b7a86379b1739d5842549b9
HayleyLei <leijl1@student.unimelb.edu.au>
2022-05-23 02:24:54 +1000

    using js to change page

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const addeditem = document.querySelectorAll("#add-to-cart");
 
+for(var i = 0; i < addeditem.length; i++) {
+    addeditem[i].addEventListener("click", myFunction1);
+}
+
+function myFunction1() {
+    console.log("clicked");
+    window.event.returnValue=false;
+    window.location = 'added.html';
+}
+
+const go_product = document.querySelectorAll(".product-link");
+for(var i = 0; i < go_product.length; i++) {
+    go_product[i].addEventListener("click", function(){
+        window.location = 'product.html';
+    });
+}
