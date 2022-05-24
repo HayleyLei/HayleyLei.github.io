621bb8d6517e6d3c8784d24b8e2123f4884e998d
HayleyLei <leijl1@student.unimelb.edu.au>
2022-05-25 01:09:42 +1000

    shopping cart page

--- main.js
+++ main.js
@@ -16,3 +16,35 @@ for(var i = 0; i < go_product.length; i++) {
         window.location = 'product.html';
     });
 }
+
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
+  }
+  
+function decreaseCount(a, b) {
+var input = b.nextElementSibling;
+var value = parseInt(input.value, 10);
+if (value > 1) {
+    value = isNaN(value) ? 0 : value;
+    value--;
+    input.value = value;
+}
+}
+
+// var quantity = document.querySelectorAll(".quan");
+// var price = document.querySelectorAll(".unit-price");
+// // var total_q = document.getElementsByClassName("total-quantity");
+// // var tatol_p = document.getElementsByClassName("total-price");
+// var q=0;
+// var p=0;
+// for (var i=0;i<quantity.length;i++) {
+//     q = q + parseInt(quantity[i].value);
+//     p = p + parseInt(quantity[i].value) * parseFloat(price[i].text);
+// }
+// document.getElementsByClassName("total-quantity").text = q;
+// document.getElementsByClassName("total-price").text = p;
+// console.log(price[0]);
